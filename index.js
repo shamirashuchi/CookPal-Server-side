80ec2a755704f95b6c5811265b6048bea424b8f1
shamirashuchi <shamirashuchi@gmail.com>
2023-05-03 23:19:22 +0600

    data added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors');
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
 const chefs = require('./data/chefs.json')
 
 
@@ -11,6 +18,13 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req,res) =>{
     res.send(chefs);
+})
+
+app.get('/chef/:id',(req,res)=>{
+   const id = parseInt(req.params.id);
+  //console.log(id);
+  const selectedchef = chefs.chefs.find(n=> n._id === id);
+  res.send(selectedchef);
 })
 
 app.listen(port, () => {
